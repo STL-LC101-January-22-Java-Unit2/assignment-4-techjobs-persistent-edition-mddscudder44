b3165c5659fe5eab11eb0405a47cd1b5d5512db7
Erin <mddscudder44@gmail.com>
2022-06-23 13:59:00 -0500

    Finished Model instructions for assignment

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
 
+    public Employer() {}
+    @Size(max = 100)
+    @NotBlank
+    private String location;
 
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
