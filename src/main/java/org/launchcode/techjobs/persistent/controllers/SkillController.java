f6916f668b27375e78221a4e65125883e9a7296d
Erin <mddscudder44@gmail.com>
2022-06-27 19:59:25 -0500

    finished SkillController.java

---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -0,0 +1,51 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("skills")
+public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute(new Skill());
+        return "skills/add";
+    }
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSKill,
+                                      Errors erros, Model model) {
+        if(erros.hasErrors()) {
+            return "skills/add";
+        }
+        skillRepository.save(newSKill);
+        return "redirect:";
+    }
+
+    @GetMapping("view/{employerId}")
+    public String displayViewSkill(Model model, @PathVariable int employerId) {
+        Optional optEmployer = skillRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+            Employer employer = (Employer) optEmployer.get();
+            model.addAttribute("employer", employer);
+            return "skills/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+
+
+}
